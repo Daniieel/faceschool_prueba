f09ebaaaacabe9a39c9591e2aa3b33f9e09d7a8b
stilexmagic <valles.cristian1992@gmail.com>
2014-11-18 19:32:24 -0300

    funcion para obtener la ip

--- application/controllers/colegio.php
+++ application/controllers/colegio.php
@@ -20,6 +20,27 @@ class Colegio extends CI_Controller {
 		$this->load->view('colegio/show', $dato);
 		$this->load->view("layouts/footer");
 	}
+	private function getClientIP() {
+
+    if (isset($_SERVER)) {
+
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+            return $_SERVER["HTTP_X_FORWARDED_FOR"];
+
+        if (isset($_SERVER["HTTP_CLIENT_IP"]))
+            return $_SERVER["HTTP_CLIENT_IP"];
+
+        return $_SERVER["REMOTE_ADDR"];
+    }
+
+    if (getenv('HTTP_X_FORWARDED_FOR'))
+        return getenv('HTTP_X_FORWARDED_FOR');
+
+    if (getenv('HTTP_CLIENT_IP'))
+        return getenv('HTTP_CLIENT_IP');
+
+    return getenv('REMOTE_ADDR');
+}
 }
 
 /* End of file welcome.php */
